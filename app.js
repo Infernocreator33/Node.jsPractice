5c8bfc4af60ea1ca6343918250e3a2e45da80c9d
Infernocreator33 <zs0877310@otc.edu>
2018-08-29 08:57:21 -0500

    updates

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+//here is a comment
+/*Multi line 
+Zachary*/
+
 console.log("hello javascript");
+
+//value or data types
+console.log("Hello from Javascript!");
+console.log("Let's do some math");
+console.log(4 + 7);
+console.log(12 / 0);
+console.log('Goodbye!');
+
+//variables
+let a = 3.14;
+console.log(a);
+const temp1 = 36.9;
+const temp2 = 37.6;
+const temp3 = 37.1;
+console.log(temp1, temp2, temp3);
+temp3 = 9;//error
